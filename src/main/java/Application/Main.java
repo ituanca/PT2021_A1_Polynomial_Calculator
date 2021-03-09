fdeeea446e5fe77a9bdb82f48d71900af49a5e42
ituanca <itu_anca@yahoo.com>
2021-03-09 11:10:03 +0200

    add class Main

--- src/main/java/Application/Main.java
+++ src/main/java/Application/Main.java
@@ -1,10 +1,11 @@
+package Application;
 import javafx.application.Application;
 import javafx.scene.Scene;
 import javafx.scene.control.Label;
 import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
 
-public class HelloFX extends Application {
+public class Main extends Application {
 
     @Override
     public void start(Stage stage) {
